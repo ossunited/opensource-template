4a75e5dc4c3ac05932a2f59d22bc9348093d409b
Jeh1928 <jbanimineni@broadsage.com>
2025-08-19 18:48:55 +0530

    feat: SPDX-FileCopyrightText copyright changed
    Signed-off-by: Jeh1928 <jbanimineni@broadsage.com>

--- scripts/code_quality.sh
+++ scripts/code_quality.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# SPDX-FileCopyrightText: Copyright (c) 2025 Broadsage
+# SPDX-FileCopyrightText: Copyright (c) 2025 Broadsage <opensource@broadsage.com>
 #
 # SPDX-License-Identifier: Apache-2.0
 
